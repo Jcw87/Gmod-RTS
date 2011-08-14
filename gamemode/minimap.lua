d36ce0b85a8c320e9e1f328aafb5fab52b23a595
John Wilson <johnnyc.wilson@verizon.net>
2011-08-14 01:19:41 +0100

    More WIP minimap stuff, now with untested usermessages

--- gamemode/minimap.lua
+++ gamemode/minimap.lua
@@ -1,17 +1,21 @@
 local CLIENT = CLIENT
 local SERVER = SERVER
+local concommand = concommand
 local file = file
 local game = game
+local hook = hook
 local player = player
 local surface = surface
 local team = team
+local usermessage = usermessage
 local util = util
+local Entity = Entity
 local ErrorNoHalt = ErrorNoHalt
 local LocalPlayer = LocalPlayer
 local Material = Material
 local pairs = pairs
 local tonumber = tonumber
-local Vector = Vector
+local tostring = tostring
 
 module("minimap")
 
@@ -39,26 +43,6 @@ local sector_width = 256
 local sector_width = 256
 
 local Ents = {}
-
-local function think()
-	for k, v in pairs(Ents) do
-		if !k:IsValid() then continue end
-		local pos = k:GetPos()
-		local angle = 0
-		if k:IsPlayer() then angle = k:EyeAngles() - 90 end
-		v.pos = Vector(pos.x, pos.y, angle)
-	end
-end
-
-function Register(ent)
-	local ent_id = ent:EntIndex()
-	Ents[ent] = {}
-end
-
-function UnRegister(ent)
-	local ent_id = ent:EntIndex()
-	Ents[ent_id] = nil
-end
 
 function LoadEmpiresScript()
 	local minimap_file = file.Read("resource/maps/"..game.GetMap()..".txt", true)
@@ -98,15 +82,124 @@ function WorldToMinimap(worldx, worldy)
 	return minix, miniy
 end
 
+if (SERVER) then
+	local function mm_request_entity(ply, cmd, args)
+		local entid = tonumber(args[1])
+		local ent = Entity(entid)
+		if !ent:IsValid() then return end
+		local enttable = Ents[entid]
+		if ent:Team() == ply:Team() || (enttable.cansee & ply:TeamMask() > 0) then
+			umsg.Start("mm_type_update", ply)
+			umsg.Short(entid)
+			umsg.String(ent:GetClass())
+			umsg.Short(ent:Team())
+			umsg.End()
+		end
+	end
+	concommand.Add("mm_request_entity", mm_request_entity)
+
+	local t_plys, t_ents
+	
+	local function ServerThink()
+		if (!t_plys) then
+			t_plys = player.GetAll()
+		end
+		local ply = t_plys[1]
+		if !ply then return end
+		if (!t_ents) then
+			t_ents = {}
+			local ent
+			for k, v in pairs(Ents) do
+				ent = Entity(k)
+				if (ply:Team() == ent:Team()) then
+					table.insert(ent)
+				end
+			end
+		end
+		local count = math.max(#t_ents, 28)
+		umsg.Start("mm_pos_update", ply)
+		umsg.Char(count)
+		for i=1, count do
+			local ent = t_ents[i]
+			umsg.Short(ent:EntIndex())
+			local pos = ent:GetPos()
+			umsg.Short(math.floor(pos.x))
+			umsg.Short(math.floor(pos.y))
+			umsg.Short(ent:IsPlayer() and (ent:EyeAngles().y - 90) or 0)
+		end
+		umsg.End()
+		for i=1, count do table.remove(t_ents, 1) end
+		if (#t_ents == 0) then
+			t_ents = nil
+			table.remove(t_plys, 1)
+		end
+		if (#t_plys == 0) then t_plys = nil end
+	end
+	hook.Add("Think", "MinimapServerThink", ServerThink)
+
+	function Register(ent)
+		local ent_id = ent:EntIndex()
+		Ents[ent] = {}
+	end
+
+	function UnRegister(ent)
+		local ent_id = ent:EntIndex()
+		Ents[ent_id] = nil
+	end
+end
+
 if (CLIENT) then
-	local Materials = {
-		["player"] = Material("vgui/player"),
-		["rts_barracks"] = Material("minimap/mmico_barracks")
-	}
+	local function mm_pos_update(um)
+		local entid, x, y, ang
+		local count = um:GetChar()
+		for i=1, count do
+			entid = um:ReadShort()
+			if (!Ents[entid]) then
+				Ents[entid] = {}
+				RunConsoleCommand("minimap_request_entity", tostring(entid))
+			end
+			local enttable = Ents[entid]
+			enttable.x = um:ReadShort()
+			enttable.y = um:ReadShort()
+			enttable.ang = um:ReadShort()
+			
+			
+		end
+	end
+	usermessage.Hook("mm_pos_update", mm_pos_update)
+	
+	local function mm_type_update(um)
+		local entid = um:ReadShort()
+		if (!Ents[entid]) then Ents[entid] = {} end
+		local enttable = Ents[entid]
+		enttable.class = um:ReadString()
+		enttable.team = um:ReadShort()
+	end
+	usermessage.Hook("mm_type_update", mm_type_update)
+	
+	local function ClientThink()
+		for k, v in pairs(Ents) do
+			local ent = Entity(k)
+			if !ent:IsValid() then continue end
+			v.class = ent:GetClass()
+			v.team = ent:Team()
+			local pos = k:GetPos()
+			v.x = pos.x
+			v.y = pos.y
+			v.ang = k:IsPlayer() and (k:EyeAngles().y - 90) or 0
+		end
+	end
+	hook.Add("Think", "MinimapClientThink", ClientThink)
+
 	function SetRenderBounds(newx, newy, neww, newh)
 		x, y, w, h = newx, newy, neww, newh
 	end
 
+	local Materials = {
+		["player"] = Material("vgui/player"),
+		["rts_barracks"] = Material("minimap/mmico_barracks")
+	}
+	
 	function Draw()
 		surface.SetDrawColor(255, 255, 255, 255)
 		surface.DrawRect(x, y, w, h)
@@ -114,21 +207,34 @@ if (CLIENT) then
 			surface.SetMaterial(map_material)
 			surface.DrawTexturedRect(x, y, w, h)
 		end
+		local ply = LocalPlayer()
+		local pos, minix, miniy, color, angle
+		--
 		for k, v in pairs(player.GetAll()) do
-			local pos = v:GetPos()
-			local minix, miniy = WorldToMinimap(pos.x, pos.y)
-			local color = team.GetColor(v:Team())
+			pos = v:GetPos()
+			minix, miniy = WorldToMinimap(pos.x, pos.y)
+			color = team.GetColor(v:Team())
 			surface.SetMaterial(Materials[v:GetClass()])
 			surface.SetDrawColor(color.r, color.g, color.b, color.a)
 			local angle = v:IsPlayer() and (v:EyeAngles().y - 90) or 0
 			surface.DrawTexturedRectRotated((minix*w) + x, (miniy*h) + y, 32, 32, angle)
 		end
-		local pos = LocalPlayer():GetPos()
-		local minix, miniy = WorldToMinimap(pos.x, pos.y)
+		--]]
+		--[[
+		for k, v in pairs(Ents) do
+			if !v.ang or !v.team or k == ply:EntIndex() then continue end
+			minix, miniy = WorldToMinimap(v.x, v.y)
+			color = team.GetColor(v.team)
+			surface.SetMaterial(Materials[v.Class])
+			surface.SetDrawColor(color.r, color.g, color.b, color.a)
+			surface.DrawTexturedRectRotated((minix*w) + x, (miniy*h) + y, 32, 32, v.ang)
+		end
+		--]]
+		pos = ply:GetPos()
+		minix, miniy = WorldToMinimap(pos.x, pos.y)
 		surface.SetMaterial(Materials.player)
 		surface.SetDrawColor(255, 255, 255, 128)
-		local angle = LocalPlayer():EyeAngles().y - 90
-		surface.DrawTexturedRectRotated((minix*w) + x, (miniy*h) + y, 48, 48, angle)
+		surface.DrawTexturedRectRotated((minix*w) + x, (miniy*h) + y, 48, 48, ply:EyeAngles().y - 90)
 		
 		return true
 	end
--- gamemode/player_extension.lua
+++ gamemode/player_extension.lua
@@ -4,6 +4,17 @@ function meta:IsCommander()
 	return self:GetNetworkedBool("IsCommander")
 end
 
+local TeamMasks = {
+	[TEAM_BLUE] = TEAM_BLUE_MASK,
+	[TEAM_RED] = TEAM_RED_MASK,
+	[TEAM_YELLOW] = TEAM_YELLOW_MASK,
+	[TEAM_GREEN] = TEAM_GREEN_MASK
+}
+
+function meta:TeamMask()
+	return TeamMasks[self:Team()] or 0
+end
+
 if SERVER then
 	function meta:SetCommander(bool)
 		if type(bool) != "boolean" then error("PLAYER:SetCommander: boolean expected, got "..type(bool), 2) end
